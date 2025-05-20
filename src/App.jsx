f3d2a120ce08e9aa4af52b6717f210c88b36d07c
34nd0m <72877329+34nd0m@users.noreply.github.com>
2025-05-21 00:37:37 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,10 @@ import { useState } from 'react';
 import BalanceBox from './components/BalanceBox';
 
 export default function App() {
-  const [rpcUrl] = useState("https://sepolia.infura.io/v3/7bf402dad7654ae3b80e3a6063876a64");
+  const [rpcUrl] = useState("https://sepolia.infura.io/v3/YOUR_INFURA_PROJECT_ID");
   const [watchMode, setWatchMode] = useState("ETH");
-  const [targetWallet, setTargetWallet] = useState("0x13CB6AE34A13a0977F4d7101eBc24B87Bb23F0d5");
-  const [tokenAddress, setTokenAddress] = useState("0x397a59aA02eB65702E5DAdDd5967bbe1979F9aC3");
+  const [targetWallet, setTargetWallet] = useState("0x0000000000000000000000000000000000000000");
+  const [tokenAddress, setTokenAddress] = useState("0x0000000000000000000000000000000000000000");
 
   return (
     <div className="p-8 font-sans">
